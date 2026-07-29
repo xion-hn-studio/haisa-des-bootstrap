caa54a5c6b98ea6f43a6a71697cf3d2f59f01980
XION-HN <xion-hn@users.noreply.github.com>
2026-07-29 11:44:09 +0000

    fix: pass GMP staging path to nettle configure to enable libhogweed
    nettle's configure couldn't find GMP in the cross-compile staging area,
    causing it to disable public key support ("Support for public key
    algorithms will be unavailable") and skip building libhogweed.so.
    This made libgnutls link fail with "unable to find library -lhogweed".
    
    Set CFLAGS/LDFLAGS to point at $STAGE_DIR$PREFIX so configure's
    AC_CHECK_LIB(__gmpn_zero_p, -lgmp) can find the staged libgmp.

--- build-system/packages/nettle/build.sh
+++ build-system/packages/nettle/build.sh
@@ -6,6 +6,12 @@ PKG_SRC_SHA256="b4c518adb174e484cb4acea54118f02380c7133771e7e9beb98a0787194ee47c
 PKG_SRC_DIR="nettle-3.10"
 
 pkg_build() {
+    # 指定 GMP 路径（来自 staging）：nettle 的 libhogweed（公钥算法）依赖 GMP，
+    # configure 找不到 GMP 会输出 "Support for public key algorithms will be
+    # unavailable" 并跳过 libhogweed.so 构建，导致下游 libgnutls 链接报
+    # "unable to find library -lhogweed"。
+    export CFLAGS="$CFLAGS -I$STAGE_DIR$PREFIX/include"
+    export LDFLAGS="$LDFLAGS -L$STAGE_DIR$PREFIX/lib"
     # --disable-assembler：Termux 环境缺少 m4/as 汇编器，禁用汇编避免探测失败
     gnu_configure \
         --disable-assembler \
